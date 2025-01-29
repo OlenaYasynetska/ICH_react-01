3466c85ecaaeaf09a933cab9849642afccbca031
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-01-29 18:40:31 +0100

    init project

---
+++ src/components/ImageComponent.jsx
@@ -0,0 +1,9 @@
+function ImageComponent() {
+    return (
+      <div>
+        <img src="https://via.placeholder.com/150" alt="Example" />
+      </div>
+    );
+  }
+
+  export default ImageComponent;
